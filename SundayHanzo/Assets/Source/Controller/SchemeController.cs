44ba624b0d2ca685b493b0d24e17ab8005505a5b
Joung Daun <i_co1022@naver.com>
2016-11-13 14:50:33 +0900

    Main화면 구성 및 GameScene으로 이동 구성

---
+++ SundayHanzo/Assets/Source/Controller/SchemeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SchemeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeToMainSchene(){
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	public void ChangeToGameScene(){
+		SceneManager.LoadScene ("GameScene");
+	}
+}
