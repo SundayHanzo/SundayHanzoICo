36b3c6aa60ae0351135e014b502104d01345740f
Joung Daun <i_co1022@naver.com>
2016-11-13 16:47:24 +0900

    화살 충돌 처리 완료

--- SundayHanzo/Assets/Source/Model/Arrow.cs
+++ SundayHanzo/Assets/Source/Model/Arrow.cs
@@ -7,10 +7,12 @@ public class Arrow : MonoBehaviour{
 	public GameObject arrow;
 	private Rigidbody arrowRigidBody;
 	public int angle;
+	private bool isStop;
 
 	// Use this for initialization
 	void Start () {
 		arrowRigidBody = arrow.GetComponent<Rigidbody> ();
+		isStop = true;
 	}
 
 	// Update is called once per frame
@@ -19,6 +21,7 @@ public class Arrow : MonoBehaviour{
 	}
 
 	public void fire(){
+		isStop = false;
 		arrowRigidBody.useGravity = true;
 		float xforce = power * Mathf.Cos (angle * Mathf.Deg2Rad);
 		float yforce = power * Mathf.Sin (angle * Mathf.Deg2Rad);
@@ -30,12 +33,22 @@ public class Arrow : MonoBehaviour{
 	}
 
 	IEnumerator moveCurve(){
-		while (true) {
+		while (!isStop) {
 			arrow.transform.LookAt (arrow.transform.position + arrowRigidBody.velocity);
 			arrow.transform.Rotate (-90, 0, 0); // angle * Mathf.Deg2Rad
 			yield return null;
 		}
 		yield return null;
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("Trigger");
+		if (col.gameObject.tag == "Target") {
+			Debug.Log ("Trigger Target");
+			isStop = true;
+			arrowRigidBody.velocity = Vector3.zero;
+			arrowRigidBody.useGravity = false;
+		}
 	}
 
 }
